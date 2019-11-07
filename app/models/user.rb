5540717db2a1b834d45ff8404e3b6b3e26f01c86
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-07 13:46:44 -0600

    create the association between event and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :events_created, class_name: 'Event', foreign_key: 'creator_id'
+
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
