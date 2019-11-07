9832f209e84a560e04615ff2fbd7aaea2eef0460
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2019-11-07 14:09:31 -0500

    Added association between event and user table.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   root 'users#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_184017) do
+ActiveRecord::Schema.define(version: 2019_11_07_190107) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date"
+    t.string "location"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +31,5 @@ ActiveRecord::Schema.define(version: 2019_11_07_184017) do
     t.string "remember_digest"
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
