c0679b7ce0e7969e67795c0e7c6d8d80ba58d089
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-12 11:45:20 -0600

    minor update in user event list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  # has_many :events_created, foreign_key: :creator_id, class_name: 'Event'
-  # has_many :events_created, foreign_key: :creator_id, class_name: 'Event'
-  has_many :events, foreign_key: 'creator_id'
+  has_many :events_created, foreign_key: :creator_id, class_name: 'Event'
+  # has_many :events, foreign_key: 'creator_id'
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
