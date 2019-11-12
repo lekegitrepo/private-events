a21047233893006cac86681e02fdcd3bdb55e251
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-12 14:02:00 -0600

    connect attendance model to user and event models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :events_created, foreign_key: :creator_id, class_name: 'Event'
+  has_many :attendances, foreign_key: :attendee_id
+  has_many :attended_events, through: :attendances
   # has_many :events, foreign_key: 'creator_id'
 
   def self.digest(string)
