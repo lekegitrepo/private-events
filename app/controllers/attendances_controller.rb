de66741ae3c65147820a59aa66cf393d92f07be4
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-13 13:29:53 -0600

    update attendance controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,20 @@
 class AttendancesController < ApplicationController
 
-  
+  def new
+    @attendance = Attendance.new
+  end
+
+  def create
+    @event = Event.find(params[:id])
+    current_user.attend(@event)
+    redirect_to @event
+  end
+
+  def cancel
+    @event = Event.find(params[:id])
+    current_user.cancel(@event)
+    redirect_to @event
+  end
 
   def upcoming
     @upcoming_events = current_user.upcoming
