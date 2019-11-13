9d7efe2dce026be873364de4c8ff3dee73eead50
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-13 14:05:33 -0600

    test for user validity

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: 'test_name', email: 'test@gmail.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
 end
