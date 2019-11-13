7a046e858ecd6bfaf8d030f962c1cb193de94e9d
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-11-13 13:45:57 -0600

    add user event status

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   has_many :attended_events, through: :attendances
   # has_many :events, foreign_key: 'creator_id'
 
+  def attend(event)
+    attendances.create(attended_event_id: event.id)
+  end
+
+  def cancel(event)
+    attendances.find_by(attended_event_id: event.id).destroy
+  end
+
+  def attending?(event)
+    event.attendees.include?(self)
+  end
+
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
