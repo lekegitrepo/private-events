4c4497b17c18f5d863104bb615c008624c68e2ff
lekegitrepo <lekedevrepo@gmail.com>
2019-11-14 15:32:52 -0600

    add test for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
     @user = User.new(name: 'test_name', email: 'test@gmail.com')
+    @user.save
   end
 
   test 'should be valid' do
     assert @user.valid?
+  end
+
+  test 'should not be valid' do
+  	@user.name = ''
+    assert_not @user.valid?
+  end
+
+  test 'nil should not be valid' do
+  	@user.name = nil
+    assert_not @user.valid?
+  end
+
+  test 'short name should not be valid' do
+  	@user.name = 'T'
+    assert_not @user.valid?
+  end
+
+  test 'long name should not be valid' do
+  	@user.name = 'T' * 25
+    assert_not @user.valid?
   end
 end
