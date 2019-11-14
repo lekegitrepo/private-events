cbe58d8a49a9a0a94d4e569ef79eee1da8ef3ca1
lekegitrepo <lekedevrepo@gmail.com>
2019-11-14 15:34:18 -0600

    add test for user and event association

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: 'test_name', email: 'test@gmail.com')
+    @user.save
+
+    @event = @user.events_created.create(name: 'Birthday Party', date: Date.today,
+                                location: 'Carribean Island',
+                                description: 'To mark Amanda and Thais birthay')
+  end
+
+  test 'should be valid' do
+    assert @event.valid?
+  end
+
+  test 'should be invalid for nil event name' do
+    @event.name = nil
+    assert_not @event.valid?
+  end
+
+  test 'should be invalid for nil event location' do
+    @event.location = nil
+    assert_not @event.valid?
+  end
+
+  test 'should be invalid for nil event description' do
+    @event.description = nil
+    assert_not @event.valid?
+  end
+
+  test 'should be invalid for nill creator' do
+    @event.creator = nil
+    assert_not @event.valid?
+  end
 end
