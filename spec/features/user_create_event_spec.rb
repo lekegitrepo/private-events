5a5a99e1b754c955bec8b4220a42a17e3ef8e5f8
lekegitrepo <lekedevrepo@gmail.com>
2019-11-17 15:01:14 -0600

    event management tests editing

--- spec/features/user_create_event_spec.rb
+++ spec/features/user_create_event_spec.rb
@@ -30,4 +30,33 @@ RSpec.describe 'User sign up and login to create new event', type: :feature do
     fill_in('Description', with: 'Birthday party')
     click_button('Create Event')
   end
+
+  scenario 'Update event' do
+    @user = User.create(name: 'test', email: 'test@example.com')
+
+    visit new_user_path
+    fill_in 'Name', with: @user.name
+    fill_in 'Email', with: @user.email
+    click_button 'Create new user'
+
+    visit '1'
+
+    click_link 'Create New Event'
+
+    expect(page).to have_content 'Create New Event'
+
+    visit new_event_path
+    fill_in('Name', with: 'Birthday')
+    fill_in('Location', with: 'Yacht')
+    fill_in('Description', with: 'Birthday party')
+    click_button('Create Event')
+
+    visit events_path
+    click_link 'Edit'
+    visit 'edit'
+    expect(page).to have_text('Editing Event')
+    expect(page).to have_button('Update Event')
+    fill_in('Location', with: 'Carribean Island')
+    click_button('Update Event')
+  end
 end
