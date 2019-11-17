db242ed480bcf95b8ca2dec680796de1210b1228
lekegitrepo <lekedevrepo@gmail.com>
2019-11-17 12:36:29 -0600

    add valid user creates new event

--- spec/features/user_create_event_spec.rb
+++ spec/features/user_create_event_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe 'User sign up and login to create new event', type: :feature do
     expect(page).to have_content('login')
     expect(page).to have_current_path(new_event_path)
   end
+
+  scenario 'Creating new event' do
+    @user = User.create(name: 'test', email: 'test@example.com')
+
+    visit new_user_path
+    fill_in 'Name', with: @user.name
+    fill_in 'Email', with: @user.email
+    click_button 'Create new user'
+
+    visit '1'
+
+    click_link 'Create New Event'
+
+    expect(page).to have_content 'Create New Event'
+
+    visit new_event_path
+    fill_in('Name', with: 'Birthday')
+    fill_in('Location', with: 'Yacht')
+    fill_in('Description', with: 'Birthday party')
+    click_button('Create Event')
+  end
 end
